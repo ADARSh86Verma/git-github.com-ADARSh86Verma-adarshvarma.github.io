ce4a534ef44a674d0a0eaaa9b159aa9674fcc0c9
ADARSh86Verma <vermaaadarsh867481@gmail.com>
2026-03-01 05:07:19 +0530

    fix: add missing newline at end of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  // ⭐ GitHub Pages ke liye
+  base: "/",
+
   server: {
     port: 5173,
     proxy: {
